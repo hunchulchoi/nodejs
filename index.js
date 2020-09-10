4348a5b8201ed01fd20ac73aaee57590bfcb74b1
choi <choihc165@gmail.com>
2020-09-10 13:49:30 +0900

    add body-parser

--- index.js
+++ index.js
@@ -13,6 +13,35 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//  body parser 
+const bodyParser = require('body-parser')
+
+// User model 사용
+const {User} = require('./models/User')
+
+// body parser 설정
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json
+app.use(bodyParser.json())
+
+
+//회원 가입
+app.post('/register', (req, res)=>{
+
+  const User = new Use(req.body)
+
+  User.save((err, userInfo)=>{
+    if(err) return res.json({success: false})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
